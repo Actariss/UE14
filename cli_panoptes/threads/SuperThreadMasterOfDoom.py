1b9fc2635ea8461cc778523ede07afb13e9af1f6
Frédéric Willems <f.willems42@gmail.com>
2022-05-17 11:41:31 +0200

    - Pre HotReload

--- cli_panoptes/threads/SuperThreadMasterOfDoom.py
+++ cli_panoptes/threads/SuperThreadMasterOfDoom.py
@@ -1,8 +1,12 @@
 import threading
 
 from queue import Queue
+from time import sleep
 
 from cli_panoptes.configuration.ConfigWrapper import ConfigWrapper
+from cli_panoptes.threads.EventThread import EventMaster
+from cli_panoptes.threads.FimThread import FimThreadMaster
+from cli_panoptes.threads.SaThread import SaThreadMaster
 from utile.network import DataReceiver
 from utile.Proto import Proto
 
@@ -19,15 +23,51 @@ class SuperThreadMasterOfDoom(threading.Thread):
         # SETUP CONFIG FROM SERVER
         self.last_sa_config = None
         self.last_fim_config = None
+        self.last_ref_images = None
 
-        self.ev_master = EventThread(q, client_configuration)
+        self.ev_master = EventMaster(q, client_configuration)
         self.ev_master.start()
 
-        while self.last_sa_config is None or self.last_fim_config is None:
+        while self.last_sa_config is None or self.last_fim_config is None or self.last_ref_images is None:
             self.last_sa_config = DataReceiver(self.config_server_port, q, Proto.LD_SA).request()
 
             self.last_fim_config = DataReceiver(self.config_server_port, q, Proto.LD_FIM).request()
 
-        self.sa_master = SaThread(q, self.last_sa_config)
-        self.fim_master = FimThread(q, self.last_fim_config)
+            self.last_ref_images = DataReceiver(self.config_server_port, q, Proto.LD_IMG).request()
+
+        self.sa_master = SaThreadMaster(q, self.last_sa_config)
+        self.fim_master = FimThreadMaster(q, self.last_fim_config, self.last_ref_images)
+
+    def run(self):
+        """Permet de démarrer le thread"""
+        self.sa_master.start()
+        self.fim_master.start()
+
+        while self.ev_master.is_alive() or self.sa_master.is_alive() or self.fim_master.is_alive():
+            sleep(self.timeout)
+
+            new_sa_config = DataReceiver(self.config_server_port, self.queue, Proto.LD_SA).request()
+
+            new_fim_config = DataReceiver(self.config_server_port, self.queue, Proto.LD_FIM).request()
+
+            new_ref_images = DataReceiver(self.config_server_port, self.queue, Proto.LD_IMG).request()
+
+            if self.last_sa_config != new_sa_config:
+                print(f'[HOT_RELOAD_CONFIG]New version of sa_config from server !!')
+                self.sa_reload(new_sa_config)
+
+            if self.last_fim_config != new_fim_config or self.last_ref_images!= new_ref_images:
+                print(f'[HOT_RELOAD_CONFIG]New version of fim_config from server !!')
+                self.fim_reload(new_fim_config, new_ref_images)
+
+        self.sa_master.join()
+        self.fim_master.join()
+        self.ev_master.join()
+
+    def sa_reload(self, new_sa_config):
+        pass
+
+    def fim_reload(self, new_fim_config, new_ref_images):
+        pass
+
 
